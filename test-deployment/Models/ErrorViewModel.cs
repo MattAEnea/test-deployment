3eea64fb44b3460927891bd3f94cbcb1a7d44d6e
Matthew Enea <enea.matthew@capgemini.com>
2022-03-16 12:02:32 -0500

    Add project files.

---
+++ test-deployment/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace test_deployment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
